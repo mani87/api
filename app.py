0f8c8699d562438381e42aa8a4ccd652a44cdeb1
Mani Kumar <mk0872191152@gmail.com>
2020-04-23 18:37:31 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+
+#init app
+app = Flask(__name__)
+
+
+# runserver
+if __name__ == '__main__':
+	app.run(debug=True)
